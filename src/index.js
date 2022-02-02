92410de25e2008617bd428ddc433003173f6aed9
Andrey Maximov <c0za@mail.ru>
2022-02-02 22:30:40 +0300

    feat: finish morse-decoder task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const MORSE_TABLE = {
+const M = {
     '.-':     'a',
     '-...':   'b',
     '-.-.':   'c',
@@ -35,10 +35,43 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ' '
 };
 
 function decode(expr) {
-    // write your solution here
+let arr = [];
+let result = [];
+let str;
+let strEl = '';
+for (let i = 0; i < expr.length; i+=10) {
+	arr.push(expr.slice(i, i+10));
+}
+
+arr.forEach(el => {
+  if (el === '**********') {
+  	str = ' ';
+    result.push(str);
+  }
+  for (let i = 0; i < el.length; i+=2) {
+    str = el.slice(i, i+2);
+    if (str === '10') {
+	strEl += '.';
+    } else if (str === '11') {
+	strEl += '-';
+    }
+    }
+    result.push(strEl);
+  strEl = '';
+});
+
+result.forEach(el => {
+	for (let key in M) {
+  		if (key === el) {
+  strEl += M[key];
+    }
+    }
+});
+return strEl;
 }
 
 module.exports = {
